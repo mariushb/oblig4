9df5e58cbe0974c25b9e70f218093b1f302e723c
Marius <mariushb@gmail.com>
2016-04-24 21:44:02 +0200

    0.3

--- Oblig4/src/TowerOfHanoi.java
+++ Oblig4/src/TowerOfHanoi.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class TowerOfHanoi {
 	static int bevegelser = 0;
+	static int rekursiv = 0;
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Oppgi antall skiver: ");
@@ -9,6 +10,7 @@ public class TowerOfHanoi {
 		System.out.println("Gjør følgende:");
 		flyttSkive(antall, 'A', 'B', 'C');
 		System.out.println("Det er utført " + bevegelser + " bevegelser");
+		System.out.println("Og " + rekursiv +  " rekursive bevegelser");
 	}
 
 	public static void flyttSkive(int antall, char fraTarn, char tilTarn, char auxTarn) {
@@ -19,6 +21,7 @@ public class TowerOfHanoi {
 			flyttSkive(antall - 1, fraTarn, auxTarn, tilTarn);
 			System.out.println("Flytt skive " + antall + " fra " + fraTarn + " til " + tilTarn);
 			flyttSkive(antall - 1, auxTarn, tilTarn, fraTarn);
+			rekursiv++;
 		}
 	}
 
